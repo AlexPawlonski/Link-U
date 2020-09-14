dc9573b19f2d9a3f1a535819cf9643ed98a0c7a9
Vincent <mezianev@hotmail.fr>
2020-09-14 16:26:06 +0200

    frontpage-test

---
+++ themes/child-link-u/front-page.php
@@ -0,0 +1,25 @@
+<?php
+// get_header();
+// Sidebar gauche
+get_sidebar('sidebar-gauche');
+// Content
+	// Sidebar gauche
+		// Et droite
+		// Sans droite
+	// Sans gauche
+		// Et droite
+		// Sans droite
+
+// Les dernières actualités
+if (have_posts()) : while (have_posts()) : the_post(); ?>
+<h1><?php the_title(); ?></h1>
+<?php the_excerpt();
+endwhile;
+endif;
+
+// Les promotions
+
+
+// Sidebar droite
+get_sidebar('sidebar-droite');
+// get_footer();
