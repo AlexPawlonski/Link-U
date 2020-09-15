007f0bf6d3ede1925c54cd960258f56a68733843
Vincent <mezianev@hotmail.fr>
2020-09-15 11:45:11 +0200

    rectifications

---
+++ themes/child-link-u/page-about.php
@@ -0,0 +1,22 @@
+<?php
+get_header();
+// Sidebar gauche
+// Content
+
+// Sidebar gauche
+?><div class="aboutContent"><?php
+
+
+
+// Les dernières actualités
+if (have_posts()) : while (have_posts()) : the_post(); ?>
+<h1>About</h1>
+<p><?php the_content(); ?></p>
+</div><!-- fermeture frontPageContent -->
+<?php
+endwhile;
+endif;
+
+// Les promotions
+// Sidebar droite
+get_footer();
